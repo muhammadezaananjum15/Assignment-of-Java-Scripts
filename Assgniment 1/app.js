4acf51e6eebe4e000fc171dbe26d9e673ab7623b
muhammadezaananjum289 <muhammadezaananjum289@gmail.com>
2025-12-19 11:43:56 +0500

    first commit

---
+++ Assgniment 1/app.js
@@ -0,0 +1,64 @@
+var fullName = 'Muhammad Ezaan Anjum';
+var age = 16;
+var city = 'Karachi';
+var isStudent = true;
+
+console.log(fullName);
+console.log(age);
+console.log(city);
+console.log(isStudent);
+
+var userName = 'Muhammad Ezaan Anjum'; // string
+var ageValue = 15; // number
+var isLoggedIn = false; // boolean
+var score = '1999'; // string
+
+var num1 = 15;
+var num2 = 19;
+
+var addition = num1 + num2;
+var subtraction = num1 - num2;
+var multiplication = num1 * num2;
+var division = num1 / num2;
+var modulus = num1 % num2;
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
+0;
+
+var result1 = 15 + 15 ** 2;
+console.log(result1);
+
+var result2 = (15 + 15) * 20;
+console.log(result2);
+
+var welcomeMessage = 'Welcome ' + fullName;
+var ageMessage = 'Your age is ' + age;
+
+alert(welcomeMessage);
+alert(ageMessage);
+
+var name1 = 'Ali';
+var fixedAge = 20;
+
+console.log(name1);
+alert('Hello World');
+
+var profileName = 'Ezaan';
+var profileAge = 16;
+var favoriteNumber = 15;
+var studentStatus = true;
+
+console.log(profileName);
+console.log(profileAge);
+console.log(favoriteNumber);
+console.log(studentStatus);
+
+var profileAlert = 'Profile Loaded Successfully';
+alert(profileAlert);
+
+//  Bonus Question Answer
+// A string stores text and it is written inside inverted commas.
+// A number stores numeric values and  it is written without inverted commas.
